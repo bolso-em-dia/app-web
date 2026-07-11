d492a2ace613a226dbd594e801b2a383ca1e303e
Daniel Arrais <danielarraiscarvalho@gmail.com>
2026-07-10 23:03:16 -0300

    Update WEB_VERSION to 1.0.15

--- src/components/ui/AppVersion.tsx
+++ src/components/ui/AppVersion.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./AppVersion.module.scss";
 
-const WEB_VERSION = "1.0.12";
+const WEB_VERSION = "1.0.15";
 
 export default function AppVersion() {
   const [apiVersion, setApiVersion] = useState<string | null>(null);
