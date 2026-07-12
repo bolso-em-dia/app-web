6ea1e08cf0f671bf2ce5e4bf346dcee350025e0d
Daniel Arrais <danielarraiscarvalho@gmail.com>
2026-07-12 15:25:17 -0300

    chore: bump version to 1.0.26

--- src/components/ui/AppVersion.tsx
+++ src/components/ui/AppVersion.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./AppVersion.module.scss";
 
-const WEB_VERSION = "1.0.25";
+const WEB_VERSION = "1.0.26";
 
 export default function AppVersion() {
   const [apiVersion, setApiVersion] = useState<string | null>(null);
