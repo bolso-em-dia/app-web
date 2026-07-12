8462ed38ee8c6f497a273e262867b1383ca33e2a
Daniel Arrais <danielarraiscarvalho@gmail.com>
2026-07-12 11:06:12 -0300

    Bump web version to 1.0.22

--- src/components/ui/AppVersion.tsx
+++ src/components/ui/AppVersion.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import styles from "./AppVersion.module.scss";
 
-const WEB_VERSION = "1.0.20";
+const WEB_VERSION = "1.0.22";
 
 export default function AppVersion() {
   const [apiVersion, setApiVersion] = useState<string | null>(null);
